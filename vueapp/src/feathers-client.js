bf91a8b9c02e9c3f48f09d53688c12b10c103d68
barnacker <samuel.tremblay@bewave.io>
2020-12-29 19:50:19 -0500

    localhost

--- vueapp/src/feathers-client.js
+++ vueapp/src/feathers-client.js
@@ -1,9 +1,9 @@
+import auth from '@feathersjs/authentication-client';
 import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio-client';
-import auth from '@feathersjs/authentication-client';
 import io from 'socket.io-client';
 
-const socket = io('https://vue.bewave.io:3030/', {
+const socket = io('http://localhost:3030/', {
   transports: ['websocket'],
 });
 
